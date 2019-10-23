2e21a31edd29e6df5e1971dff03891e57a879713
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-10-23 17:31:03 -0400

    more config

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -16,6 +16,7 @@ import dj_database_url
 import dotenv
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
    dotenv.load_dotenv(dotenv_file)
@@ -158,3 +159,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import django_heroku
 django_heroku.settings(locals())
+
+del DATABASES['default']['OPTIONS']['sslmode']
